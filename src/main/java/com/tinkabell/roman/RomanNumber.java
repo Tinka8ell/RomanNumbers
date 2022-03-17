95c94cfab2172b03d490300e94909da916f50329
Mark Ingamells <mark.ingamells@gmail.com>
2022-03-17 11:14:38 +0000

    Create basic roman numeral creator and minimal test cases

--- src/main/java/com/tinkabell/roman/RomanNumber.java
+++ src/main/java/com/tinkabell/roman/RomanNumber.java
@@ -2,24 +2,58 @@ package com.tinkabell.roman;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Scanner;
+
 /**
  * A roman number is a natural number expressed as roman numerals.
  * This implementation is therefore wraps an Integer,
  * but limited to only positive values (not zero),
  * and the parse function will accept a string of roman numerals.
+ *
+ * From Wikipedia:
+ * Roman numerals are a numeral system that originated in ancient Rome and remained
+ * the usual way of writing numbers throughout Europe well into the Late Middle Ages.
+ * Numbers in this system are represented by combinations of letters from the Latin alphabet.
+ * Modern style uses seven symbols, each with a fixed integer value:
+ *
+ * Symbol	I	V	X	L	C	D	M
+ * Value	1	5	10	50	100	500	1000
+ *
+ * Analysis:
+ * Given that there is no concept of zero, or even negative numbers
+ * (it was after all a "tally" system), the smallest number is 1 and the
+ * largest should be 4000-1 (as there is no '5' of thousands), but this
+ * implementation will allow up to 9 units ('I', 'X', 'C' & 'M') to be
+ * valid (if not correct) so gives a maximum of 10k-1.
  */
 public class RomanNumber {
 
     public Integer value;
+    private final boolean wasRoman = false;
+
+    private static final int MAX_INT_VALUE = 4000 - 1;
+    private static final int MIN_INT_VALUE = 1;
+
+    public static RomanNumber MAX_VALUE = new RomanNumber(4000 - 1);
+    public static RomanNumber MIN_VALUE = new RomanNumber(1);
+
+    private static final String[][] numerals = {
+            {"I", "V", "X"},
+            {"X", "L", "C"},
+            {"C", "D", "M"},
+            {"M", "", ""},
+    };
 
     /**
      */
-    private RomanNumber(String s) throws NumberFormatException {
+    public RomanNumber(String s) throws NumberFormatException {
         value = Integer.parseInt(s);
     }
 
-    private RomanNumber(int i){
+    private RomanNumber(int i)
+            throws NumberFormatException{
         value = i;
+        if (value < MIN_INT_VALUE || value > MAX_INT_VALUE) throw new NumberFormatException("Value out of range");
     }
 
     /**
@@ -42,7 +76,86 @@ public class RomanNumber {
         return new RomanNumber(Integer.parseInt(s));  // for now just accept arabic digits
     }
 
+    public String asRoman () {
+        int rest = value;
+        int [] powers = new int[4];
+        for (int i = 0; i < powers.length; i++) {
+            powers[i] = rest % 10;
+            rest /= 10;
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = powers.length - 1; i >= 0 ; i--) {
+            result.append(makeRoman(powers[i], numerals[i]));
+        }
+        return result.toString();
+    }
+
+    private static String makeRoman(int power, String[] numeral) {
+        String result = "";
+        switch (power){
+            case 5:
+                if (numeral[1].equals(""))
+                    result = numeral[1].repeat(power);
+                else
+                    result = numeral[1];
+                break;
+            case 4:
+                if (numeral[1].equals(""))
+                    result = numeral[0].repeat(power);
+                else
+                    result = numeral[0] + numeral[1];
+                break;
+            case 9:
+                if (numeral[2].equals(""))
+                    result = numeral[0].repeat(power);
+                else
+                    result = numeral[0] + numeral[2];
+                break;
+            case 0:
+                // nothing to add
+                break;
+            default:
+                if (power < 5){
+                    result = numeral[0].repeat(power);
+                } else {
+                    if (numeral[1].equals(""))
+                        result = numeral[0].repeat(power);
+                    else
+                        result = numeral[1] + numeral[0].repeat(power - 5);
+                }
+                break;
+        }
+        return result;
+    }
+
+    public String asArabic(){
+        return value.toString();
+    }
+
+    public String toString(){
+        return wasRoman ?
+                "Roman number " + asRoman() + " is " + asArabic() + " in arabic notation":
+                "Arabic number " + asArabic() + " is " + asRoman() + " in Roman Numerals";
+    }
+
+    public static String prompt(Scanner scanner, String s){
+        System.out.println(s);
+        return scanner.nextLine();
+    }
+
     public static void main(String[] args) {
+        /* Wanted to use Console, but not supported under IDEA!
+        Console console = System.console();
+        System.console() returns as null!
+         */
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Roman Number Translator");
+        System.out.println("Enter an empty line to exit");
+        String input = prompt(scanner, "Please enter a Roman Numeral");
+        while (input.trim().length() > 0){
+            RomanNumber number = RomanNumber.parseRoman(input.trim());
+            System.out.println(number);
+            input = prompt(scanner,"Please enter another Roman Numeral");
+        }
     }
 }
--- src/test/java/com/tinkabell/roman/RomanNumberTest.java
+++ src/test/java/com/tinkabell/roman/RomanNumberTest.java
@@ -2,14 +2,43 @@ package com.tinkabell.roman;
 
 import org.junit.jupiter.api.Test;
 
+import static com.tinkabell.roman.RomanNumber.parseRoman;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RomanNumberTest {
 
     @Test
-    public void checkParseRoman(){
+    public void checkOnes(){
         // Arrange
-        RomanNumber number = RomanNumber.parseRoman("1");
+        String [] ones = {
+                "I",
+                "II",
+                "III",
+                "IIII"
+        };
+        String [] expected = {
+                "1",
+                "2",
+                "3",
+                "4,"
+        };
+
+        // Act
+        String[] results = new String[ones.length];
+        for (int i = 0; i < results.length; i++) {
+            results[i] = parseRoman(ones[i]).asArabic();
+        }
+
+        // Assert
+        for (int i = 0; i < ones.length; i++) {
+            assertEquals(expected[i], results[i]);
+        }
+    }
+
+    @Test
+    public void checkParseRoman() {
+        // Arrange
+        RomanNumber number = parseRoman("1");
 
         // Act
         int result = 1;
@@ -18,4 +47,79 @@ class RomanNumberTest {
         assertEquals(result, number.value);
     }
 
+    @Test
+    public void checkAsRoman() {
+        // Arrange
+        String [] numbers = {
+                "1",
+                "3",
+                "4",
+                "5",
+                "8",
+                "9",
+                "10",
+                "2021",
+                "1959"
+        };
+        String [] expected = {
+                "I",
+                "III",
+                "IV",
+                "V",
+                "VIII",
+                "IX",
+                "X",
+                "MMXXI",
+                "MCMLIX"
+        };
+
+        // Act
+        String[] results = new String[numbers.length];
+        for (int i = 0; i < results.length; i++) {
+            results[i] = parseRoman(numbers[i]).asRoman();
+        }
+
+        // Assert
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(expected[i], results[i]);
+        }
+    }
+
+    @Test
+    public void checkAsArabic() {
+        // Arrange
+        String [] numbers = {
+                "I",
+                "III",
+                "IV",
+                "V",
+                "VIII",
+                "IX",
+                "X",
+                "MMXXI",
+                "MCMLIX"
+        };
+        String [] expected = {
+                "1",
+                "3",
+                "4",
+                "5",
+                "8",
+                "9",
+                "10",
+                "2021",
+                "1959"
+        };
+
+        // Act
+        String[] results = new String[numbers.length];
+        for (int i = 0; i < results.length; i++) {
+            results[i] = parseRoman(numbers[i]).asArabic();
+        }
+
+        // Assert
+        for (int i = 0; i < numbers.length; i++) {
+            assertEquals(expected[i], results[i]);
+        }
+    }
 }
